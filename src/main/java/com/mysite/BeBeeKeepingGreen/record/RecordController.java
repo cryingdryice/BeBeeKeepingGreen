88185b43a4cc314808f18ad84871e883cafd4743
ParkJiwon <parkjiwon0821@naver.com>
2024-04-14 16:36:47 +0900

    기록 삭제구현 완료

--- src/main/java/com/mysite/BeBeeKeepingGreen/record/RecordController.java
+++ src/main/java/com/mysite/BeBeeKeepingGreen/record/RecordController.java
@@ -4,13 +4,16 @@ import com.mysite.BeBeeKeepingGreen.user.SiteUser;
 import com.mysite.BeBeeKeepingGreen.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
@@ -46,5 +49,13 @@ public class RecordController {
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.recordService.create(recordForm.getContent(), siteUser);
         return "redirect:/record/list";
+    }
+
+    @PreAuthorize("isAuthenticated()") // 로그인해야만 접속가능
+    @GetMapping("/list/delete/{id}")
+    public String recordDelete(Principal principal, @PathVariable("id") Integer id){
+        HiveRecord hiveRecord = this.recordService.getHiveRecord(id);
+        this.recordService.delete(hiveRecord);
+        return "redirect:/";
     }
 }
--- src/main/java/com/mysite/BeBeeKeepingGreen/record/RecordService.java
+++ src/main/java/com/mysite/BeBeeKeepingGreen/record/RecordService.java
@@ -1,11 +1,13 @@
 package com.mysite.BeBeeKeepingGreen.record;
 
+import com.mysite.BeBeeKeepingGreen.DataNotFoundException;
 import com.mysite.BeBeeKeepingGreen.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -22,5 +24,18 @@ public class RecordService {
         hr.setContent(content);
         hr.setOwner(owner);
         this.recordRepository.save(hr);
+    }
+
+    public void delete(HiveRecord hiveRecord){
+        this.recordRepository.delete(hiveRecord);
+    }
+
+    public HiveRecord getHiveRecord(Integer id){
+        Optional<HiveRecord> record = this.recordRepository.findById(id);
+        if(record.isPresent()){
+            return record.get();
+        }else{
+            throw new DataNotFoundException("기록 존재하지 않음");
+        }
     }
 }
